8d68510c7698c0a40fc7420cfbb2506521e0fb8b
Hla Thue Prue Marma <hlathuepruemarma@Hlas-MacBook-Air.local>
2026-07-01 14:29:41 +0300

    Add to update profile funtionality

--- auth/AuthProvider.jsx
+++ auth/AuthProvider.jsx
@@ -1,17 +1,63 @@
-import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updatePassword } from 'firebase/auth';
 import React, { createContext, useEffect, useState } from 'react';
 import { auth } from '../firebase/Firebase.config';
-
+import { GoogleAuthProvider } from "firebase/auth";
 
 export const AuthContext = createContext()
+export const googleProvider = new GoogleAuthProvider()
 const AuthProvider = ({children}) => {
 
     const [loading,setLoading]= useState(true);
     const [user,setUser]= useState(null)
 
+    const signUpUser =(email,password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    // signin user
+    const signInUser =(email,password)=>{
+
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+      
+    }
+
+    // google sign in
+
+    const googleSingInUser =()=>{
+        setLoading(true)
+        return signInWithPopup(auth,googleProvider)
+    }
+
     const signOutUser =()=>{
-        loading(true)
+        setLoading(true)
         return signOut(auth)
+    }
+
+    // set up forget password
+
+    const forgetPasswordUser =(email)=>{
+        setLoading(true)
+        return sendPasswordResetEmail(auth,email)
+    }
+
+
+    // create user email and password
+
+    const registrationUser=(email,password)=>{
+        return createUserWithEmailAndPassword (auth,email,password)
+    }
+
+    // update profile
+   
+    const updateProfile=(name,photoUrl)=>{
+
+        const profile ={
+            displayName: name,
+            photoURL: photoUrl,
+        }
+        return updateProfile(profile)
     }
 
     useEffect(()=>{
@@ -29,6 +75,12 @@ const AuthProvider = ({children}) => {
          signOutUser,
          user,
          loading,
+         signInUser,
+         googleSingInUser,
+         signUpUser,
+         forgetPasswordUser,
+         registrationUser,
+         
     }
     return (
         <AuthContext.Provider value={authInfo}>
